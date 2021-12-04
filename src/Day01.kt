a60907a458e21ecd47e087e6691d10e21ad81b0e
Matthew Glover <matthewglover@me.com>
2021-12-04 12:05:43 +0000

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,50 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val totalReadings = input.size
+
+        var increasingDepthCount = 0
+
+        for (index in 0 until totalReadings) {
+            if (index == 0) {
+                continue
+            }
+
+            val previousDepth = input[index - 1].toInt()
+            val currentDepth = input[index].toInt()
+
+            if (currentDepth > previousDepth) {
+                increasingDepthCount += 1
+            }
+        }
+
+        return increasingDepthCount
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val interval = 3
+        val totalReadings = input.size
+
+        var increasingDepthCount = 0
+
+        for (index in 0..totalReadings) {
+            if (index < interval || index >= totalReadings) {
+                continue
+            }
+
+            val previousDepth = input.subList(index - interval, index).sumOf { it.toInt() }
+            val currentDepth = input.subList(index - interval + 1, index + 1).sumOf { it.toInt() }
+
+            if (currentDepth > previousDepth) {
+                increasingDepthCount += 1
+            }
+        }
+
+        return increasingDepthCount
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
